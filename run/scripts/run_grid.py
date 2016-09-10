cd9331a5708356c525f02bea4cf2d0f9d74c4b29
Qi Zeng <qi.zeng@cern.ch>
2016-09-10 14:08:17 +0200

    job production for 00-00-00

--- run/scripts/run_grid.py
+++ run/scripts/run_grid.py
@@ -1,12 +1,19 @@
 import os
-from samples import *
+import samples
+# # test purpose
+# sampleList = ["mc15_13TeV.341577.PowhegPy8EG_AZNLOCTEQ6L1_WplusenuH_H125_a20a20_bbbb.merge.AOD.e4118_s2608_s2183_r7772_r7676"]  # for test only
+# nEventsPerJob = 1000
+# version = "test0"
 
-# sampleList = _MC_Signal + _MC_ttbar
-sampleList = ["mc15_13TeV.341577.PowhegPy8EG_AZNLOCTEQ6L1_WplusenuH_H125_a20a20_bbbb.merge.AOD.e4118_s2608_s2183_r7772_r7676"]  # for test only
+# # signal samples
+# sampleList = samples._MC_Signal
+# nEventsPerJob = 5000
+# version = "00-00-00"
 
-# config
-nEventsPerJob = 1000
-version = "test0"
+# ttbar samples
+sampleList = samples._MC_ttbar
+nEventsPerJob = 10000
+version = "00-00-00"
 
 # now loop over them
 for sampleName in sampleList:
@@ -15,6 +22,8 @@ for sampleName in sampleList:
 	
 	inDS_split = inDS.split(".")
 	inDS_split[-2] = "DAOD_SJET1"
+	inDS_split.pop(-1)
+	inDS_split.pop(-2)
 	outDS = "user.qzeng." + ".".join(inDS_split) + "." + version
 
 	config = {
@@ -23,6 +32,9 @@ for sampleName in sampleList:
 	  "nEventsPerJob": nEventsPerJob,
 	}
 
-	cmd = "pathena --userNewTRF --trf \"Reco_tf.py --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = \\\"BTagCalibRUN12-08-18\\\"' --reductionConf SJET1 --maxEvents '{nEventsPerJob}' --skipEvents %SKIPEVENTS --inputAODFile=%IN --outputDAODFile %OUT.DAOD_SJET1.pool.root\" --skipScout --nEventsPerJob={nEventsPerJob} --inDS {inDS} --outDS {outDS}".format(**config)
+	cmd = "pathena --useNewTRF --trf \"Reco_tf.py --preExec 'rec.doApplyAODFix.set_Value_and_Lock(True);from BTagging.BTaggingFlags import BTaggingFlags;BTaggingFlags.CalibrationTag = \\\"BTagCalibRUN12-08-18\\\"' --reductionConf SJET1 --maxEvents '{nEventsPerJob}' --skipEvents %SKIPEVENTS --inputAODFile=%IN --outputDAODFile %OUT.DAOD_SJET1.pool.root\" --skipScout --nEventsPerJob={nEventsPerJob} --inDS {inDS} --outDS {outDS}".format(**config)
 
+	print "\n"
 	print cmd
+	print "\n"
+	os.system(cmd)
