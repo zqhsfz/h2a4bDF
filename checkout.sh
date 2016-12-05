eb3311c03ce8eef517ef6a96b55f76379ec927af
Qi Zeng <qi.zeng@cern.ch>
2016-12-05 09:16:56 +0100

    A major update on pkg with updated releases and tags. Include ExKt using ghost associated tracks

--- checkout.sh
+++ checkout.sh
@@ -3,20 +3,20 @@ echo " "
 echo "checking out packages ..."
 echo " "
 pkgco.py JetCalibTools-00-04-67
-pkgco.py JetTagTools-01-00-96
-pkgco.py BTagging-00-07-64
-pkgco.py DerivationFrameworkFlavourTag-00-01-55
-pkgco.py DerivationFrameworkTop-00-03-03
+pkgco.py ParticleJetTools-00-03-33-08
+pkgco.py DerivationFrameworkFlavourTag-00-01-84
+pkgco.py DerivationFrameworkJetEtMiss-00-03-56
+pkgco.py DerivationFrameworkTop
 pkgco.py DerivationFrameworkCore
-pkgco.py AODFix
+pkgco.py BTagging-00-07-63-05
+pkgco.py JetTagTools-01-00-96-04
+pkgco.py AODFix-00-03-29
 
 # apply patches
 echo " "
 echo "applying patches ..."
 echo " "
-cp patches/DerivationFrameworkProdFlags.py PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/DerivationFrameworkProdFlags.py
-cp patches/TOPQCommonSelection.py PhysicsAnalysis/DerivationFramework/DerivationFrameworkTop/python/TOPQCommonSelection.py
-cp patches/AODFix_r207.py Reconstruction/AODFix/python
+patch -N Reconstruction/AODFix/python/AODFix_r207.py < patches/AODFix_r207.patch
+patch -N PhysicsAnalysis/DerivationFramework/DerivationFrameworkCore/python/DerivationFrameworkProdFlags.py < patches/DerivationFrameworkProdFlags.patch
+patch -N PhysicsAnalysis/DerivationFramework/DerivationFrameworkTop/python/TOPQCommonSelection.py < patches/TOPQCommonSelection.patch
 cp patches/SJET1.py PhysicsAnalysis/DerivationFramework/DerivationFrameworkTop/share
-cp patches/ExKtbb*.cxx PhysicsAnalysis/JetTagging/JetTagTools/src/
-cp patches/ExKtbb*.h PhysicsAnalysis/JetTagging/JetTagTools/JetTagTools/
